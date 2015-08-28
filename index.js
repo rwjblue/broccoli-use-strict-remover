a99e5215ab6b698a8507658a15d07a0ff458151a
Robert Jackson <robert.w.jackson@me.com>
2015-08-27 22:39:19 -0400

    Use broccoli-persistent-filter as base class.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Filter = require('broccoli-filter');
+var Filter = require('broccoli-persistent-filter');
 
 function UseStrictRemover (inputTree, _options) {
   if (!(this instanceof UseStrictRemover)) {
@@ -8,6 +8,11 @@ function UseStrictRemover (inputTree, _options) {
   }
 
   var options = _options || {};
+  if (!options.hasOwnProperty('persist')) {
+    options.persist = true;
+  }
+  Filter.call(this, inputTree, options);
+
   var matcher = options.matcher || /REMOVE_USE_STRICT: true/g;
 
   delete options.matcher;
@@ -21,6 +26,10 @@ UseStrictRemover.prototype.constructor = UseStrictRemover;
 
 UseStrictRemover.prototype.extensions = ['js'];
 UseStrictRemover.prototype.targetExtension = 'js';
+
+UseStrictRemover.prototype.baseDir = function() {
+  return __dirname;
+};
 
 UseStrictRemover.prototype.processString = function(content) {
   var output = content;
